bab4aa39b4d298dbc6eede7f69a00f295ac6bf60
jbcima <me@jeremycimafonte.com>
2013-06-23 23:30:10 -0400

    facebook web view auto-add, hop on dis.

--- brotap/ViewController.m
+++ brotap/ViewController.m
@@ -14,10 +14,16 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+- (IBAction)toggleButtonClick:(id)sender {
+    if (_fbWebView.hidden == NO)
+    {
+        _fbWebView.hidden = YES;
+    }
+    else
+    {
+        _fbWebView.hidden = NO;
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +32,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidLoad
+{
+    _fbWebView.delegate = self;
+    //_fbWebView.hidden = YES;
+
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSString *fullURL = @"http://m.facebook.com/danield";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_fbWebView loadRequest:requestObj];
+
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSString *addFriend = [webView stringByEvaluatingJavaScriptFromString:@""
+                           "var brotap_url = document.querySelectorAll('div[data-sigil=\"add-friend-button-wrapper\"] a')[0].href;"
+                           
+                           "function brotap_addFriend(brotap_url) {"
+                           "	var req = new XMLHttpRequest();"
+                           "	req.open('GET', brotap_url, false);"
+                           "	req.send(null);"
+                           "	if(req.status == 200)"
+                           "		return \"yes\";"
+                           "	else"
+                           "		return \"no\";"
+                           "}"
+                           "brotap_addFriend(brotap_url);"];
+    NSLog(@"addFriend: %@", addFriend);
+
+
+}
+
+
 @end
+
